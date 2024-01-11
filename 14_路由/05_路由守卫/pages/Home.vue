750c6f028446988fa220a2a8451ccfceff12d360
lifeibiao <1739195020@qq.com>
2024-01-11 16:14:35 +0800

    路由守卫

---
+++ 14_路由/05_路由守卫/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>我是Home</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link replace class="list-group-item active" active-class="active" to="/home/news">news</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item active" active-class="active" to="/home/message">message</router-link>
+        </li>
+      </ul>
+
+      <keep-alive :include="['News'] ">
+        <router-view></router-view>
+      </keep-alive> 
+      
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name:'Home'
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 14_路由/05_路由守卫/pages/News.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <ul>
+            <li :style="{opacity}">
+                欢迎李飞彪
+            </li>
+            <li>
+                news01<input type="text">
+            </li>
+            <li>
+                news01<input type="text">
+            </li>
+            <li>
+                news01<input type="text">
+            </li>
+            <li>
+                news01<input type="text">
+            </li>
+        </ul>
+      </div>
+</template>
+
+<script>
+    export default {
+        name:'News',
+
+        data(){
+            return{
+                opacity:1
+            }
+        },
+
+        // mounted(){
+        //     this.timer = setInterval(() => {
+        //         this.opacity -= 0.01
+        //         if(this.opacity <= 0) this.opacity = 1
+        //     },20)
+        // },
+
+        // beforeDestroy(){
+        //     console.log("NEWs组件即将被销毁了")
+        //     clearInterval(this.timer)
+        // },
+
+        activated(){
+            console.log("news组件被激活了")
+            this.timer = setInterval(() => {
+                this.opacity -= 0.01
+                if(this.opacity <= 0) this.opacity = 1
+            },20)
+        },
+        deactivated(){
+            console.log("news组件失活了")
+            clearInterval(this.timer)
+        }
+    }
+</script>
+
+<style>
+
+</style>
