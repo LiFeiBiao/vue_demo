bcfe52407717e5bca798bbc6de1d72152a37f03c
lifeibiao <1739195020@qq.com>
2024-01-11 14:05:55 +0800

    activated和deactivated两个组件专用生命周期钩子

---
+++ 15_activated和deactivated钩子/components/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+    <div >
+        <div >
+          <h2>VueRouterDemo</h2>
+
+          <button @click="back">后退</button>
+          <button @click="forward">前进</button>
+
+          <button @click="test">测试一下go方法</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'banner',
+        methods:{
+            back(){
+                this.$router.back()
+            },
+            forward(){
+                this.$router.forward()
+            },
+            test(){
+                this.$router.go(-2) 
+            }
+        }
+
+
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 15_activated和deactivated钩子/pages/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <!-- <li>消息编号{{ $route.query.id }}</li>
+    <li>消息标题{{ $route.query.title }}</li> -->
+
+    <!-- <li>消息编号{{ $route.params.id }}</li>
+    <li>消息标题{{ $route.params.title }}</li> -->
+
+    <li>消息编号{{ id }}</li>
+    <li>消息标题{{ title }}</li>
+
+    <li>a:{{ a }}</li>
+    <li>b:{{ b }}</li>
+  </ul>
+</template>
+  
+<script>
+    export default {
+        name:'Detail',
+        
+        // props:['a','b'],
+
+        props:['id','title']
+
+    }
+</script>
+
+<style>
+
+</style>
